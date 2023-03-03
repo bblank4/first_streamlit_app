05b0f47577633f76c06db70f69206016ecff7895
bblank4 <92455884+bblank4@users.noreply.github.com>
2023-03-03 13:51:26 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,9 +27,9 @@ streamlit.dataframe(fruits_to_show)
 
 # create the repeatable code block (called a function)
 def get_fruitvice_data(this_fruit_choice):
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-    return(fruityvice_normalized)
+    return fruityvice_normalized
 
 #New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
@@ -40,7 +40,6 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-    
     
 except URLError as e:
     streamlit.error()
